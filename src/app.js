e834af7368b606214e425aae235e5b6172836479
netoxdw <neto.guevara.tapia@gmail.com>
2021-11-16 19:59:13 -0600

    tratando de corregir controlles y eliminando directorio db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 
 import express from 'express';
-import controller from './controlles/index.controlles';
 import Campos from './routes/campos.routes'
 const app = express();
 
--- src/controlles/index.controlles.js
+++ src/controlles/index.controlles.js
@@ -1,17 +1,3 @@
-const controller ={}
-const connection= require ('../dbconnection/database')
 
-controller.index = async(req, res) => {
-    try{
-    const title = 'index desde el servidor con una variable'
-    await connection()
-    console.log('conection ok')
-    res.send('index', {title})
-    }catch(err){
-        console.error(err)
-    }
-}
-(controller.index)
-export default controller
 
 
